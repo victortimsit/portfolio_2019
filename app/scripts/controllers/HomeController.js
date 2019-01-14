663f60508f85e551020613c889483fac691d1cd1
vtimsit <victortimsit.contact@gmail.com>
2019-01-14 22:17:57 +0100

    :sparkles: HomeController

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -2,9 +2,15 @@ class HomeController
 {
   constructor()
   {
+    this.$ = 
+    {
+      projectsTitles: document.querySelector('.projectsTitles__list'),
+      projectsPreviews: document.querySelector('.projectsPreviews'),
+    }
+
     this.data
+
     this._sendRequest()
-    console.log('init')
   }
 
   _sendRequest()
@@ -16,9 +22,50 @@ class HomeController
     request.responseType = 'json'
     request.send()
 
-    request.onload = function() {
-      this.data = request.response
-      console.log(this.data.projects)
+    request.onload = () => {
+      const data = request.response
+
+      this._craftProjectsTitles(data)
+      
+      new ScrollBar()
+    }
+  }
+
+  _craftProjectsTitles(_data)
+  {
+    const data = _data.projects
+
+    for(let i = 0; i < data.length; i++)
+    {
+      // Create project DOM
+      const projectDOM = 
+      {
+        title: document.createElement('li'),
+        previewItem: document.createElement('li'),
+        category: document.createElement('div'),
+        link: document.createElement('a'),
+        previewImage: document.createElement('div'),
+        image: document.createElement('img')
+      }
+
+      // Add data
+      projectDOM.title.innerText = data[i].title
+      projectDOM.category.innerText = data[i].category
+      projectDOM.image.setAttribute('src', data[i].thumbnail)
+
+      // Add class
+      projectDOM.previewItem.classList.add('projectsPreviews__item')
+      projectDOM.category.classList.add('projectsPreviews__category')
+      projectDOM.previewImage.classList.add('projectsPreviews__image')
+
+      // AppendChild
+      projectDOM.previewItem.appendChild(projectDOM.category)
+      projectDOM.previewItem.appendChild(projectDOM.link)
+      projectDOM.link.appendChild(projectDOM.previewImage)
+      projectDOM.previewImage.appendChild(projectDOM.image)
+
+      this.$.projectsPreviews.appendChild(projectDOM.previewItem)
+      this.$.projectsTitles.appendChild(projectDOM.title)
     }
   }
 }
--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,3 +1,2 @@
 new Octagon()
-new ScrollBar()
 new HomeController()
