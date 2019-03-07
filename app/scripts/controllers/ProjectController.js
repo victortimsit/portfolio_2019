091ba00eaee5d0425c62e51945fb06e3f3963eb9
vtimsit <victortimsit.contact@gmail.com>
2019-03-07 15:56:08 +0100

    :art: Add videos

--- app/scripts/controllers/ProjectController.js
+++ app/scripts/controllers/ProjectController.js
@@ -194,8 +194,10 @@ class ProjectController
           if(_data.content[i][key] == 'video') 
           {
             img = document.createElement('video')
+            img.setAttribute('autoplay')
+            img.setAttribute('loop')
           } 
-          
+
           const p = document.createElement('p')
 
           p.classList.add('section__description')
